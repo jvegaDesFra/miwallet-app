a975bee3a294d62b8380f806ec33daf173228a75
jvegaDesFra <jose.juan.vega@outlok.com>
2022-10-12 00:26:01 -0500

    script para notarize

--- electron/scripts/notarize.js
+++ electron/scripts/notarize.js
@@ -1,18 +1,26 @@
 require('dotenv').config();
 const { notarize } = require('electron-notarize');
-
+console.log("notarize");
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
-  if (electronPlatformName !== 'darwin') {
-    return;
-  }
+    const { electronPlatformName, appOutDir } = context;
+    console.log("notarize", electronPlatformName);
+    console.log("notarize", appOutDir);
 
-  const appName = context.packager.appInfo.productFilename;
+    if (electronPlatformName !== 'darwin') {
+        return;
+    }
 
-  return await notarize({
-    appBundleId: 'com.miwallet.medico',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: '',//process.env.APPLEID,
-    appleIdPassword:'' //process.env.APPLEIDPASS,
-  });
+    const appName = context.packager.appInfo.productFilename;
+    console.log("notarize", appName);
+    notarize({
+        appBundleId: 'com.miwallet.medico',
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId: 'ibrito@britech.mx',//process.env.APPLEID,
+        appleIdPassword: 'bntj-samd-ethz-vvfa', //process.env.APPLEIDPASS,
+        teamId: "VK84D5T3UA"
+    }).then(result => {
+        console.log(result);
+    }).catch(error => {
+        console.log(error);
+    });
 };
